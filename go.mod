a53a04a0f0c833f79431e11ab81c6d1aeabb461b
thejaswa <thejaswaroop@gmail.com>
2023-09-05 17:37:57 +0530

    puppy go and mod

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thejaswa/puppy
 
 go 1.21.0
+
+require github.com/thejaswa/dog v0.0.0-20230905115842-670abcbc89e7
--- puppy.go
+++ puppy.go
@@ -1,9 +1,22 @@
 package puppy
 
+import (
+	"github.com/thejaswa/dog"
+)
+
 func Bark() string {
 	return "Bow!"
 }
 
 func Braks() string {
 	return "Bow! Bow! Bow!"
+}
+
+func BigBark() string {
+	//return "BowBOW"
+	return dog.BigDogBark(Bark())
+}
+func BigBarks() string {
+	//return "BowBOW"
+	return dog.BigDogBark(Braks())
 }
